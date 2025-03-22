4637af6048308cdf7f6298020aee367715f187c5
Manuel Henriquez <manuelhm1993@gmail.com>
2025-03-21 23:32:54 -0400

    Snippets, son las ayudas o bloques de código comunes que permiten hacer modificaciones con tabs

--- vsc-curso/js/main.js
+++ vsc-curso/js/main.js
@@ -1,8 +1,5 @@
-const sumar = (numA, numB) => numA + numB;
+const saludo = (nombre) => {
+    console.log(`Hola ${nombre}`);
+};
 
-let numA = 5;
-let numB = 2;
-let numC = 6;
-let resultado = sumar(numA, numB);
-
-console.log(`${numA} + ${numB} = ${resultado}`);
+saludo("Manuel Henriquez");
